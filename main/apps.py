4b8dcebe944c5e0dd4d4e0463270a80dabc67fc3
Spencer M. Tasa <spencertasatech@gmail.com>
2025-07-07 22:10:39 +0200

    Refactor ready method to clarify conditions for running background email watcher

--- main/apps.py
+++ main/apps.py
@@ -1,11 +1,16 @@
 from django.apps import AppConfig
 import os
+import sys
 
 class MainConfig(AppConfig):
     name = 'main'
 
     def ready(self):
         from main.email_watcher import start_background_email_watcher
-        # Ensure this only runs in the main process, not the autoreloader
-        if os.environ.get("RUN_MAIN") == "true":
+
+        # Run only in main process, not migrations, shell, or Celery worker
+        if (
+            os.environ.get("RUN_MAIN") == "true"  # dev mode: runserver autoreload check
+            or "gunicorn" in sys.argv[0]          # prod mode: e.g., gunicorn
+        ):
             start_background_email_watcher()
