2bd8d01e86846a4d5a26886ac98408c0bf982df7
Spencer M. Tasa <spencertasatech@gmail.com>
2025-07-09 14:08:48 +0200

    DJANGO_ENV=production

--- main/apps.py
+++ main/apps.py
@@ -2,13 +2,18 @@
 import os
 from django.apps import AppConfig
 
+# ✅ Global flag — module level
+email_watcher_started = False
+
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
 
     def ready(self):
-        if os.environ.get("DJANGO_ENV") != "production":
+        global email_watcher_started
+
+        if os.environ.get("DJANGO_ENV") != "production" and not email_watcher_started:
             from .email_watcher import start_background_email_watcher
-            if os.environ.get("RUN_MAIN") == "true":
-                print("[✓] Email watcher thread started by main app ready method")
-                start_background_email_watcher()
+            print("[✓] Email watcher thread started by main app ready method")
+            start_background_email_watcher()
+            email_watcher_started = True
